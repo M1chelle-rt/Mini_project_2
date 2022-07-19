bd9b53bda51479e6cb1072c3487ba4ffc281d6a8
Michelle Thomas <Michellerose.thms@gmail.com>
2022-07-19 10:17:45 -0500

    Initial Commit

---
+++ Mini_project_2/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Mini_project_2
+//
+//  Created by Guest on 7/15/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBAction func nextBtn(_ sender: Any) {
+    }
+}
+
---
+++ Mini_project_2UITests/Mini_project_2UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Mini_project_2UITestsLaunchTests.swift
+//  Mini_project_2UITests
+//
+//  Created by Guest on 7/15/22.
+//
+
+import XCTest
+
+class Mini_project_2UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
